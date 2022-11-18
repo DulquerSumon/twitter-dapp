ddb93a003f6514bd5f56d531d50f89afbb66fe41
DulquerSumon <m.dulquer.sumon@gmail.com>
2022-11-18 22:29:41 +0600

    initial commit

---
+++ deploy/01-deploy-twitter.js
@@ -0,0 +1,18 @@
+const { network } = require("hardhat");
+const { verify } = require("../utils/verify");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const twitter = await deploy("Twitter", {
+    from: deployer,
+    log: true,
+    args: [],
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+  if (network.config.chainId == 5 && process.env.ETHERSCAN_API_KEY) {
+    await verify(twitter.address, []);
+  }
+  console.log(`Contract deployed at : ${twitter.address}`);
+};
